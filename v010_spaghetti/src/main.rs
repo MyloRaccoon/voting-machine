581988b5462e1384ce35164460049d0dbb533d9e
Mylo <joachim.fevre@gmail.com>
2025-01-23 17:30:57 +0100

    continue tp1 (not finished, bugged)

--- v010_spaghetti/src/main.rs
+++ v010_spaghetti/src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::Error;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use tokio::io::{self, AsyncBufReadExt, BufReader};
 
 fn print_commands() {
@@ -10,61 +10,73 @@ fn print_commands() {
     println!(" - q : quit");
 }
 
-fn print_voters(voters: &mut HashMap<String, String>) {
+fn print_voters(voters: &mut BTreeMap<String, String>) {
     println!("Voters : ");
-    for voter in voters.keys() {
-        println!(" - {} voted for {}", voter, voters.get(voter).unwrap());
+    for (voter, candidate) in voters {
+        println!(" - {voter} voted for {candidate}");
     }
 }
 
-fn print_scores(scores: &mut HashMap<String, u32>) {
+fn print_scores(scores: &mut BTreeMap<String, u32>) {
     println!("Candidates for best Game Engine :");
-
-    for candidate in scores.keys() {
-        println!(" - {} : {}", candidate, scores.get(candidate).unwrap())
+    for (candidate, score) in scores {
+        println!(" - {candidate} : {score} votes");
     }
 }
 
 fn process_voting(
-    voters: &mut HashMap<String, String>,
+    voters: &mut BTreeMap<String, String>,
     voter: String,
     candidate: String,
-    scores: &mut HashMap<String, u32>,
+    scores: &mut BTreeMap<String, u32>,
 ) {
     if voters.contains_key(&voter) {
         println!("Voter \"{}\" already voted !", voter);
         return;
     }
 
-    if scores.contains_key(&candidate) {
-        println!("{} vote for {} !", voter, candidate);
-        scores.insert(candidate, scores.get(&candidate).unwrap() + 1);
-        voters.insert(voter, candidate);
-    } else if candidate == String::from("") {
+    if candidate == String::from("") {
         println!("{} voted white !", voter);
-        voters.insert(voter, String::from("white"));
-    } else {
+        voters.insert(voter, String::from("White"));
+        scores.insert(
+            "Null".to_string(),
+            scores.get(&"White".to_string()).unwrap() + 1,
+        );
+    } else if !scores.contains_key(&candidate)
+        || candidate == String::from("Null")
+        || candidate == String::from("White")
+    {
         println!(
             "\"{}\" is not a candidate, {} voted null !",
             candidate, voter
         );
-        voters.insert(voter, String::from("null"));
+        voters.insert(voter, String::from("Null"));
+        scores.insert(
+            "Null".to_string(),
+            scores.get(&"Null".to_string()).unwrap() + 1,
+        );
+    } else {
+        println!("{} vote for {} !", voter, candidate);
+        scores.insert(candidate.clone(), scores.get(&candidate).unwrap() + 1);
+        voters.insert(voter, candidate.clone());
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let mut voters: HashMap<String, String> = HashMap::new();
-    let mut condidates = vec![
+    let mut voters: BTreeMap<String, String> = BTreeMap::new();
+    let candidates = vec![
         String::from("Godot"),
         String::from("Unity"),
         String::from("Unreal Engine"),
         String::from("Game Maker Studio"),
+        String::from("White"),
+        String::from("Null"),
     ];
-    let mut scores: HashMap<String, u32> = HashMap::new();
+    let mut scores: BTreeMap<String, u32> = BTreeMap::new();
 
-    for candidate in &condidates {
-        scores.insert(*candidate, 0);
+    for candidate in &candidates {
+        scores.insert(candidate.clone(), 0);
     }
 
     println!(" ~ Welcome ~");
@@ -74,7 +86,23 @@ async fn main() -> Result<(), Error> {
     let mut lines = BufReader::new(io::stdin()).lines();
 
     while let Some(input) = lines.next_line().await? {
-        match input.trim_end() {
+        match input.split_whitespace().next().unwrap_or("") {
+            "vote" => {
+                let mut ite = input.split_whitespace();
+                ite.next();
+                let voter = ite.next().unwrap_or("");
+                if voter != "" {
+                    let candidate = ite.next().unwrap_or("");
+                    process_voting(
+                        &mut voters,
+                        voter.to_string(),
+                        candidate.to_string(),
+                        &mut scores,
+                    );
+                } else {
+                    println!("Command \"vote\" gets 2 args [voter] [candidate]");
+                }
+            }
             "voters" => print_voters(&mut voters),
             "scores" => print_scores(&mut scores),
             "q" => {
